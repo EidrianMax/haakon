99784f32f0390a163dced8c9a296c4f0a8cd8e55
Adrian <eidrian.uct@gmail.com>
2024-02-09 11:42:16 +0100

    add models

---
+++ api/database/models/schemas/game.js
@@ -0,0 +1,42 @@
+const { Schema, Types: { ObjectId } } = require('mongoose')
+
+const game = new Schema({
+  name: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  released: {
+    type: Date
+  },
+  backgroundImage: {
+    type: String
+  },
+  screenshots: [{
+    type: String
+  }],
+  platforms: [
+    {
+      type: ObjectId,
+      ref: 'Platform'
+    }
+  ],
+  genres: [
+    {
+      type: ObjectId,
+      ref: 'Genre'
+    }
+  ],
+  score: {
+    type: Number
+  },
+  website: {
+    type: String
+  },
+  isFav: {
+    type: Boolean
+  }
+})
+
+module.exports = game
