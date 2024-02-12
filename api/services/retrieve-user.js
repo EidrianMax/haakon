0612f161bfd6dc1e8a758bffc85809d39960e29e
Adrian <eidrian.uct@gmail.com>
2024-02-12 15:47:42 +0100

    refactor and migrate to jest retrieve-user

--- api/services/retrieve-user.js
+++ api/services/retrieve-user.js
@@ -2,23 +2,14 @@ const { models: { User } } = require('@haakon/api-database')
 const { validateId } = require('./helpers/validators')
 const { NotFoundError } = require('@haakon/api-errors')
 
-function retrieveUser (id) {
+async function retrieveUser (id) {
   validateId(id)
 
-  return (async () => {
-    const user = await User.findById(id).lean()
-    if (!user) throw new NotFoundError(`user with id ${id} not found`)
+  const user = await User.findById(id)
 
-    user.id = user._id.toString()
+  if (!user) throw new NotFoundError(`user with id ${id} not found`)
 
-    delete user._id
-
-    delete user.password
-
-    delete user.__v
-
-    return user
-  })()
+  return user
 }
 
 module.exports = retrieveUser
