e94e4271754c0f978b9a42de5bd0adce2a2aad56
Adrian <eidrian.uct@gmail.com>
2024-02-13 13:13:57 +0100

    add delete-fav-game

---
+++ api/services/delete-fav-game.js
@@ -0,0 +1,26 @@
+const { validateId } = require('./helpers/validators')
+const { NotFoundError } = require('@haakon/api-errors')
+const { models: { User, Game } } = require('@haakon/api-database')
+
+async function pushFavGame (userId, gameId) {
+  validateId(userId)
+  validateId(gameId)
+
+  const user = await User.findById(userId)
+
+  if (!user) throw new NotFoundError(`user with id ${userId} not found`)
+
+  const game = await Game.findById(gameId)
+
+  if (!game) throw new NotFoundError(`game with id ${gameId} not found`)
+
+  const newFavGames = user.favGames.filter(favGame => favGame.toString() !== gameId)
+
+  user.favGames = newFavGames
+
+  const userSaved = await user.save()
+
+  return userSaved.favGames
+}
+
+module.exports = pushFavGame
