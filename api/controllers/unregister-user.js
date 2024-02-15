fb6cacf53b8f4d6f186caf54e3849f67deadaf54
Adrian Alvarado <eidrian.uct@gmail.com>
2024-02-15 15:18:36 +0100

    refactor unregister-user controller

--- api/controllers/unregister-user.js
+++ api/controllers/unregister-user.js
@@ -1,15 +1,15 @@
 const { unregisterUser } = require('@haakon/api-services')
 const { validateAuthorizationAndExtractPayload } = require('./helpers')
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const { headers: { authorization }, body: { password } } = req
 
   try {
     const { sub: id } = validateAuthorizationAndExtractPayload(authorization)
 
-    unregisterUser(id, password)
-      .then(() => res.status(201).send())
-      .catch(next)
+    await unregisterUser(id, password)
+
+    res.status(201).send()
   } catch (error) {
     next(error)
   }
