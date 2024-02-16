b71406ba6174607d709bd95b4f73f59428aa7e46
Adrian Alvarado <eidrian.uct@gmail.com>
2024-02-16 13:27:02 +0100

    refactor serch-games app service

--- app/src/services/search-games.js
+++ app/src/services/search-games.js
@@ -1,29 +1,24 @@
+import { validateQuery } from './helpers/validators'
 import { API_URL } from './constants'
 
 /**
- * Signs up a user in the application.
+ * Search games by query
  *
  * @param {string} query The query wath will be search
  */
 
 export default async function searchGames (query) {
-  if (typeof query !== 'string') throw new TypeError(query + ' is not a string')
-  if (!query.trim().length) throw new Error('query is empty or blank')
-  if (query.trim() !== query) throw new Error('blank spaces around query')
+  validateQuery(query)
 
   const res = await fetch(`${API_URL}/games?q=${query}`, {
     method: 'GET'
   })
 
-  const { status } = res
-
-  if (status === 200) {
-    return await res.json()
-  } else if (status === 404) {
+  if (!res.ok) {
     return []
-  } else if (status === 400) {
-    const { error } = await res.json()
+  }
 
-    throw new Error(error)
-  } else throw new Error('unknown error')
+  const games = await res.json()
+
+  return games
 }
