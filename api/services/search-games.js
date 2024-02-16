9bd47f2560e7e3f7b2a6c05396498f6884baf032
Adrian Alvarado <eidrian.uct@gmail.com>
2024-02-16 13:24:06 +0100

    refactor search-game

--- api/services/search-games.js
+++ api/services/search-games.js
@@ -1,6 +1,5 @@
 const { models: { Game } } = require('@haakon/api-database')
 const { validateQuery } = require('./helpers/validators')
-const { NotFoundError } = require('@haakon/api-errors')
 
 async function searchGames (query) {
   validateQuery(query)
@@ -11,7 +10,7 @@ async function searchGames (query) {
     .populate('platforms', { name: 1 })
     .populate('genres', { name: 1 })
 
-  if (!games.length) throw new NotFoundError('no game found with that match')
+  if (!games.length) return []
 
   const gamesToReturn = games.map(game => ({
     id: game.id,
